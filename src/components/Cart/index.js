9228ad6070c0e20f67549de353492ad06251a113
jhemi-cristina <jhemilycristina@gmailo.com>
2022-08-23 20:40:10 -0300

    adjusts component cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,21 +1,55 @@
-import { Button, Container, Product, Title, TotalPrice } from "./styles";
+import {
+  Button,
+  CardConfig,
+  Container,
+  Finalization,
+  ImageProd,
+  Price,
+  Product,
+  Qtd,
+  Title,
+  TitleImage,
+  TotalPrice,
+  Value,
+} from "./styles";
 import { AiFillCloseCircle } from "react-icons/ai";
 import Card from "components/Card";
+import { apple } from "assets/apple.svg";
 
-export default function Cart({ handleModal }) {
+export default function Cart({ handleModal, imageUrl, title, value }) {
   return (
     <Container>
       <Product>
         <Title>
-          Carrinho de compras
+          Carrinho <br />
+          de compras
           <button onClick={handleModal}>
             <AiFillCloseCircle />
           </button>
         </Title>
-        <Card></Card>
-        <TotalPrice></TotalPrice>
+        <Card>
+          <CardConfig>
+            <img src={imageUrl} width={50} height={50} />
+            <TitleImage>{title}</TitleImage>
+            <Qtd></Qtd>
+            <Price>{value}</Price>
+          </CardConfig>
+        </Card>
+
+        <Card>
+          <CardConfig>
+            <img src={imageUrl} width={50} height={50} />
+            <TitleImage>{title}</TitleImage>
+            <Qtd></Qtd>
+            <Price>{value}</Price>
+          </CardConfig>
+        </Card>
       </Product>
-      <Button></Button>
+      <Finalization>
+        <TotalPrice>Total:</TotalPrice>
+        <Value>R$799</Value>
+      </Finalization>
+      <Button>Finalizar Compra</Button>
     </Container>
   );
 }
--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -1,25 +1,97 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 export const Container = styled.div`
-  position: absolute;
-  width: 486px;
-  height: 1024px;
-  left: 954px;
-  top: 0px;
+  position: fixed;
+  top: 0;
+  right: -10px !important;
+  min-height: 100vh !important;
+  display: flex;
+  z-index: 999;
+  flex-direction: column;
+  justify-content: space-between;
 
   background: #0f52ba;
   box-shadow: -5px 0px 6px rgba(0, 0, 0, 0.13);
 `;
 
-export const Product = styled.div``;
+export const Product = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 export const Title = styled.h3`
   font-size: 27px;
   color: #ffffff;
   font-weight: 700;
   top: 36px;
+  padding-bottom: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 100px;
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+
+    svg {
+      width: 38px;
+      height: 38px;
+    }
+  }
 `;
 
-export const TotalPrice = styled.div``;
+export const CardConfig = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
 
-export const Button = styled.div``;
+export const ImageProd = styled(Image)`
+  width: 20px;
+  height: 20px;
+`;
+
+export const TitleImage = styled.h4``;
+
+export const Qtd = styled.div``;
+
+export const Price = styled.div``;
+
+export const Finalization = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 50px;
+`;
+
+export const TotalPrice = styled.div`
+  font-weight: 700;
+  font-size: 28px;
+  color: #ffffff;
+`;
+
+export const Value = styled.div`
+  font-weight: 700;
+  font-size: 28px;
+  color: #ffffff;
+`;
+
+export const Button = styled.div`
+  width: 486px;
+  height: 97px;
+
+  bottom: 0px;
+  background: #000;
+  font-weight: 700;
+  font-size: 28px;
+  color: #ffffff;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`;
